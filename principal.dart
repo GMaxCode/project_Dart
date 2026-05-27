cf2197effdf179c6fec50ba2e5d4dc4b79b1e6bf
Guilherme Maximo <guilhermegomesmaximo@gmail.com>
2026-05-26 23:32:30 -0300

    codigo quase perfeito

--- principal.dart
+++ principal.dart
@@ -25,16 +25,10 @@ void main() async {
             final int chatId = message['chat']['id'];
             final String texto = message['text'];
 
+            
+
             print('Mensagem recebida: $texto');
 
-            // 1. Verificação do comando /start
-            if (texto == '/start') {
-              String boasVindas = 'Olá, seja bem-vindo ao DartBot! 👋\n\n'
-                  'Somos um verificador de CPF.\n'
-                  'Por favor, digite seu CPF: (exemplo: 000.000.000-00)';
-              
-              await enviarMensagem(baseUrl, chatId, boasVindas);
-            } 
             // 2. Lógica de validação (só processa se NÃO for um comando)
             else if (!texto.startsWith('/')) {
               String resposta = validarCPF(texto) 
